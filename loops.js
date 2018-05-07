c37082fa1bff9299495675856238e69a51e5f538
Adrian V <avarela@elpuig.xeill.net>
2018-05-07 13:10:01 +0200

    Animacion

---
+++ loops.js
@@ -0,0 +1,32 @@
+var elements = [3, 7, 9, 18, 24, 5, 7, 27, 32, 35, 42, 47];
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+function greaterThan18(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+document.getElementById("1").innerHTML = greaterThan18(elements);
+
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+// y que además son pares
+function greaterThan18Odd(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+document.getElementById("2").innerHTML = greaterThan18Odd(elements);
+
+// Retorna la suma de los números que son estrictamente mayores que 18
+// y que además son pares y que se encuentran en posiciones impares del vector
+function greaterThan18OddEvenPositions(elements) {
+
+  // Escriviu aquí el vostre codi:
+  return 0;
+}
+
+
+document.getElementById("3").innerHTML = greaterThan18OddEvenPositions(elements);
